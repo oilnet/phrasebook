036cb608a6ad003d756ab51873891e0a3cbc8832
J. R. Schmid <jrs+git@weitnahbei.de>
2016-01-21 23:25:23 +0100

    change can't be accompanied by up or down; changing to up and down only.

--- db/migrate/20160121215050_move_phrases_text_contents_to_translations.rb
+++ db/migrate/20160121215050_move_phrases_text_contents_to_translations.rb
@@ -8,11 +8,21 @@ class MovePhrasesTextContentsToTranslations < ActiveRecord::Migration
         language: phrase.language
       )
     end
-  end
-
-  def change
     remove_column :phrases, :text, :text
     remove_column :phrases, :recording_data, :binary
     remove_column :phrases, :language, :string
+  end
+
+  def down
+    add_column :phrases, :text, :text
+    add_column :phrases, :recording_data, :binary
+    add_column :phrases, :language, :string
+    # Possibly incomplete or wrong and dangerous in any case!
+    Translation.where(language: 'de').all.each do |translation|
+      translation.phrase.update_attribute(:text, translation.text)
+      translation.phrase.update_attribute(:recording_data, translation.recording_data)
+      translation.phrase.update_attribute(:language, translation.language)
+      translation.destroy
+    end
   end
 end
