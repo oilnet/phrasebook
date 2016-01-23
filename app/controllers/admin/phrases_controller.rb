b44f08d3007d0b724805f8fab28eb8680fc4c252
J. R. Schmid <jrs+git@weitnahbei.de>
2016-01-23 01:41:05 +0100

    Admin kann jetzt jeweilige Phrase anzeigen.

--- app/controllers/admin/phrases_controller.rb
+++ app/controllers/admin/phrases_controller.rb
@@ -1,5 +1,15 @@
 class Admin::PhrasesController < Admin::AdminController
+  before_filter :find_phrases
   def index
+  end
+  
+  def show
+    @phrase = Phrase.find params[:id]
+  end
+  
+  private
+  
+  def find_phrases
     @phrases = Phrase.all
   end
 end
