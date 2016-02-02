619672768f43816431ca2aacbfd17f73b1e1ddbe
J. R. Schmid <jrs+git@weitnahbei.de>
2016-02-02 12:34:59 +0100

    Vorbereitungen Datenbankänderungen.

--- db/migrate/20150920160805_rename_audio_data_to_recording.rb
+++ db/migrate/20150920160805_rename_audio_data_to_recording.rb
@@ -1,6 +1,5 @@
 class RenameAudioDataToRecording < ActiveRecord::Migration
   def change
     rename_column :translations, :audio_data, :recording_data
-    remove_column :translations, :rec_filename
   end
 end
