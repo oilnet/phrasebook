b725ed9f8f3fa07956b07170bbfd520cd2966bc3
J. R. Schmid <jrs+git@weitnahbei.de>
2016-02-05 03:49:59 +0100

    Anfänge einer User-Ansicht für den Admin.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,17 @@
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < Admin::AdminController
+  before_filter :find_objects
+  
+  def index
+    # Siehe before_filter.
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  private
+  
+  def find_objects
+    @users = User.all
+  end
 end
