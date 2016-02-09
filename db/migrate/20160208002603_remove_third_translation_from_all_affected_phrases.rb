f888cd394ec18221bdc88ef19bd909a11f183303
J. R. Schmid <jrs+git@weitnahbei.de>
2016-02-09 10:24:58 +0100

    Printing removed Translations to stdout.

--- db/migrate/20160208002603_remove_third_translation_from_all_affected_phrases.rb
+++ db/migrate/20160208002603_remove_third_translation_from_all_affected_phrases.rb
@@ -4,6 +4,7 @@ class RemoveThirdTranslationFromAllAffectedPhrases < ActiveRecord::Migration
     ps=[]; Phrase.all.each {|p| ps << p if p.translations.count > 2}
     ps.each do |p|
       loop do
+        puts "Phrase #{p.id}: removing #{p.translations.last.inspect}."
         p.translations.last.destroy
         break if p.translations.count == 2
       end
