8e6487b47a16e333bb99a0f41679970fa5c7bb85
J. R. Schmid <jrs+git@weitnahbei.de>
2016-02-09 11:28:25 +0100

    Limiting access to controller actions appropriately.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,18 @@ Rails.application.routes.draw do
   root_path = 'phrases#index'
 
   scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
-    resources :searches, :translations, :phrases, :user_sessions, :pages
+    resources :pages,         only: [:index, :show]
+    resources :phrases,       only: [:index, :show]
+    resources :searches,      only: [:index, :create]
+    resources :translations,  only: [:show]
+    resources :user_sessions, only: [:new, :create]
+
     resources :users, only: [:new, :create] do
       member do
         get :activate
       end
     end
+
     get '/sign_up',  to: 'users#new',             as: :sign_up
     get '/sign_in',  to: 'user_sessions#new',     as: :sign_in
     get '/sign_out', to: 'user_sessions#destroy', as: :sign_out
