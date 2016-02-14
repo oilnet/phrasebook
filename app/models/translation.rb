0b4718093a62aa4a9d0575a5bfc9a29ad36f4a3d
J. R. Schmid <jrs+git@weitnahbei.de>
2016-02-14 19:28:46 +0100

    Fixed bug with conversion of audio recording to MP3.

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -25,21 +25,22 @@
 # t.datetime "created_at"
 # t.datetime "updated_at"
 
+require 'fileutils'
+
 class Translation < ActiveRecord::Base
   belongs_to :phrase
 
+  before_validation :recording_data_is_mp3
+  
   validates :language, 
     presence: true, 
     uniqueness: {scope: :phrase, message: "Die Phrase hat bereits eine Übersetzung in der gewählten Sprache."}
-  validate  :recording_data_is_mp3
   
-  default_scope {order(text: :asc)}
+  default_scope {order(language: :asc)}
   scope :language, ->(language) {where("language = ?", language)}
  
   def recording_filename
-    f  = "#{Rails.application.class.parent_name.downcase}-phrase_#{phrase_id}-translation_#{id}-#{language}"
-    f += ".mp3"
-    f
+    "#{Rails.application.class.parent_name.downcase}-phrase_#{phrase_id}-translation_#{id}-#{language}.mp3"
   end
   
   def human_readable_language
@@ -49,34 +50,37 @@ class Translation < ActiveRecord::Base
   private
   
   def recording_data_is_mp3
-    if recording_data
-      begin
-        # If getUserData was used to record the audio, the
-        # data will have been written into a hidden input.
-        if recording_data[0,4] == 'data'
-          logger.debug "*** Recording data seems to be fresh base64..."
-          return if recording_data.blank? # It might just be the empty input...
-          tempfile = Tempfile.new('recording_', :encoding => 'ascii-8bit')
-          recording_data = Base64.decode64(recording_data.split(',').last)
-          logger.debug "*** Decoded base64 string."
-          tempfile.write recording_data
-          logger.debug "*** Wrote #{tempfile.path}."
-          # Do the conversion, but only if input is WAV.
-          if recording_data[0,4] == 'RIFF'
-            ffmpeg = FFMPEG::Movie.new(tempfile.path)
-            mp3_file = "#{tempfile.path}.mp3"
+    begin
+      if recording_data && recording_data[0,4] == 'data'
+        logger.debug "*** Recording data seems to be fresh base64..."
+        binary_data = Base64.decode64(recording_data.split(',').last)
+        recording_data = binary_data
+        logger.debug "*** Decoded base64 string."
+        FileUtils.mkdir_p 'tmp/recordings'
+        # Do the conversion, but only if input is WAV.
+        if recording_data[0,4] == 'RIFF'
+          wav_file = "tmp/recordings/#{id}.wav"
+          mp3_file = wav_file.gsub(/\.wav/, '.mp3')
+          File.open(wav_file, 'wb') do |file|
+            file.write recording_data
+            logger.debug "*** Wrote #{wav_file}."
+            ffmpeg = FFMPEG::Movie.new(file.path)
             ffmpeg.transcode(mp3_file)
-            self.recording_data = File.read(mp3_file)
-            logger.debug "*** Converted to MP3."
-            tempfile.close
-            tempfile.unlink
-            logger.debug "*** Deleted tempfile."
-          end
-        end
-      rescue Exception => e
-        logger.error e.inspect
-        errors.add(:recording_data, "konnte wegen eines Fehlers bei der Umwandlung zu MP3 nicht gespeichert werden (#{e})") # TODO: i18n!
+            logger.debug "*** Wrote #{mp3_file}"
+            # TODO: Figure out why in the world simply /setting/ the attributes
+            # doesn't work. Really, really shouldn't have to use update_attributes
+            # here ... even with protection from an endless loop I'm not sure whether
+            # it is safe.
+            self.update_attributes(recording_data: File.read(mp3_file))
+            logger.debug "*** Saved recording as MP3."
+          end            
+        else
+          raise 'User-Client hat keine WAV-Daten geschickt.' # TODO: i18n!
+        end          
       end
+    rescue Exception => e
+      logger.error e.inspect
+      errors.add(:recording_data, "konnte wegen eines Fehlers bei der Umwandlung zu MP3 nicht gespeichert werden (#{e})") # TODO: i18n!
     end
   end
 end
